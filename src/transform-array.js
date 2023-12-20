0404743184a2a7faf226e6e608762d2d55df410a
Matvey Ishmametev <ishmametev_m@mail.ru>
2023-12-20 17:48:42 +0500

    feat: add solution for sum digits task

--- src/transform-array.js
+++ src/transform-array.js
@@ -15,7 +15,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 function transform(/* arr */) {
   throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+  // let transformArr = [];
+  // if (!Array.isArray(arr)) {
+  //   throw new Error('\'arr\' parameter must be an instance of the Array!')
+  // } else {
+  //   for (let i = 0; i < arr.length; i += 1) {
+  //     if (arr[i] === '--double-next') {
+  //       arr[i] = arr[i+1];
+  //       transformArr.push(arr[i])
+  //     } else if (arr[i] === '--double-prev') {
+  //       arr[i] = arr[i-1];
+  //       transformArr.push(arr[i])
+  //     } else if (arr[i] === '--discard-next') {
+  //       arr.splice(i, 1)
+  //      } else if (arr[i] === '--discard-prev') {
+  //       arr.splice((i - 1), 1)
+  //     } else {
+  //       transformArr.push(arr[i])
+  //     }
+  //   }
+  // }
+  // return transformArr;
 }
 
 module.exports = {
