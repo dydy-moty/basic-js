947ed75a1a6932237ccaf6396fe17177d861c3b4
Matvey Ishmametev <ishmametev_m@mail.ru>
2023-12-23 15:13:05 +0500

    feat: add solution for what season task

--- src/what-season.js
+++ src/what-season.js
@@ -11,26 +11,31 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // const MONTH = date.getMonth();
-  // if (!date) {
-  //   return 'Unable to determine the time of year!';
-  // } else  {
-  //   if (!date instanceof Date) {
-  //     throw new Error("Invalid date!");
-  //   } else {
-  //     if (MONTH < 5 && MONTH > 1) {
-  //       return 'spring';
-  //     } else if ( MONTH < 8 && MONTH > 4 ) {
-  //       return 'summer'
-  //     } else if ( MONTH < 11 && MONTH > 7 ) {
-  //       return 'fall'
-  //     } else if ( MONTH > 10 || MONTH === 0 || MONTH === 1 ) {
-  //       return 'winter'
-  //     }
-  //   }
-  // }
+function getSeason(date) {
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  try {
+      const MONTH = date.getMonth();
+
+      if (!(date instanceof Date)) {
+        throw new TypeError('Invalid date!');
+      } else if (isNaN(date)) {
+        throw new TypeError('Invalid date!');
+      }
+      if (MONTH < 5 && MONTH > 1) {
+        return 'spring';
+      } else if ( MONTH < 8 && MONTH > 4 ) {
+        return 'summer'
+      } else if ( MONTH < 11 && MONTH > 7 ) {
+        return 'fall'
+      } else if ( MONTH > 10 || MONTH === 0 || MONTH === 1 ) {
+        return 'winter'
+      }
+
+    } catch (error) {
+      throw new TypeError('Invalid date!');
+    }
 }
 
 module.exports = {
